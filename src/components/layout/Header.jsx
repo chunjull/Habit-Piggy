142aa47ca669fedcad900aecbe854852ee27f2f1
chunjull <chunjuchein@gmail.com>
2023-09-11 15:02:13 +0800

    fix: header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,33 +4,27 @@ import Icon from '../../assets/images/icon.png';
 const Header = () => {
   return (
     <header className="header">
-      <div className="bg-primary">
-        <div className="container">
-          <nav className="navbar navbar-expand-lg py-5">
-            <div className="container d-flex justify-content-between">
-              <h1><Link className="navbar-brand fs-3 lobster-two text-white" to="./"><img src={Icon} alt="habit-piggy-icon" className="align-bottom" />Habit Piggy</Link></h1>
-              <div>
-                <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                  <ul className="navbar-nav">
-                    <li className="nav-item">
-                      <Link className="nav-link text-white fs-5 p-0" to="./Articles">推薦文章</Link>
-                    </li>
-                    <li className="nav-item ps-16">
-                      <Link className="nav-link text-white fs-5 p-0" to="./Subscribe">訂閱方案</Link>
-                    </li>
-                    <li className="nav-item ps-16">
-                      <Link className="nav-link text-white fs-5 p-0" to="./Login">會員登入</Link>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </div>
+      <div className="navbar navbar-expand-lg navbar-light bg-primary py-5">
+        <nav className="container">
+          <h1><Link className="navbar-brand fs-3 lobster-two text-white" to="./"><img src={Icon} alt="habit-piggy-icon" className="align-bottom" />Habit Piggy</Link></h1>
+          <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i className="bi bi-list text-white fs-1"></i>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-lg-auto mb-lg-0 fs-6 pb-1 pb-lg-0 mt-3">
+              <li className="nav-item pt-5 pt-lg-0 ps-12 ps-lg-0">
+                <Link className="nav-link fs-5 p-0" to="./Articles">推薦文章</Link>
+              </li>
+              <li className="nav-item pt-5 pt-lg-0 ps-12 ps-lg-16">
+                <Link className="nav-link fs-5 p-0" to="./Subscribe">訂閱方案</Link>
+              </li>
+              <li className="nav-item pt-5 pt-lg-0 ps-12 ps-lg-16">
+                <Link className="nav-link fs-5 p-0" to="./Login">會員登入</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>  
     </header>
   );
 }
