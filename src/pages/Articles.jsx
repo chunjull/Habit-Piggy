18db909531998d45eeab22f48fdfaafbcf142e9b
chunjull <chunjuchein@gmail.com>
2023-09-12 15:24:24 +0800

    fix: articles layout

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -19,11 +19,12 @@ const Articles = () => {
                   <img src="https://images.unsplash.com/photo-1466193341027-56e68017ee2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80" className="w-100" alt="article picture" />
                 </div>
                 <div className="col-lg-7">
-                    <div className="card-body p-0 px-lg-5">                      <h5 className="card-title fs-6 fs-lg-3 fw-bold mb-0 mb-lg-7">《原子習慣》幫你打造更好版本的自己</h5>
+                    <div className="card-body p-0 px-lg-5">                      
+                    <h5 className="card-title fs-6 fs-md-3 fw-bold mb-0 mb-lg-7">《原子習慣》幫你打造更好版本的自己</h5>
                     <p className="card-text d-none d-lg-block fs-6 mb-7">每個人都有自己的目標與願望，但要達成這些目標並不總是容易的事情。我們往往會在改變的路上遇到困難，可能會失去動力，甚至回到過去的舊習慣中。然而，《原子習慣》的作者詹姆斯・克⋯⋯</p>
                     <div className="d-none d-lg-block border-bottom border-darkgray"></div>
-                    <div className="d-none d-lg-flex justify-content-end mt-5">
-                    <Link to="/Article" className="card-link link-black fs-6 fw-bold">觀看文章<i className="bi bi-arrow-right text-primary ps-1"></i></Link>
+                    <div className="d-flex justify-content-end mt-5">
+                      <Link to="/Article" className="card-link link-black fs-7 fs-lg-6">觀看文章<i className="bi bi-arrow-right text-primary ps-1"></i></Link>
                     </div>
                   </div>
                 </div>
@@ -44,11 +45,11 @@ const Articles = () => {
                   </div>
                   <div className="col-lg-7">
                     <div className="card-body p-0 px-lg-5">
-                      <h5 className="card-title fs-6 fs-lg-3 fw-bold mb-0 mb-lg-7">培養新習慣前，先用５個步驟破除心魔</h5>
+                      <h5 className="card-title fs-6 fs-md-3 fw-bold mb-0 mb-lg-7">培養新習慣前，先用５個步驟破除心魔</h5>
                       <p className="card-text d-none d-lg-block fs-6 mb-7">近年專門研究習慣和生產力的學者指出，專注養成習慣的過程，遠比訂立計畫重要。Habit Piggy 編輯團隊為你整理出 5 個步驟，幫助你破除心魔、建立好習慣，並且讓你可以獲得更好的自己。⋯⋯</p>
                       <div className="d-none d-lg-block border-bottom border-darkgray"></div>
-                      <div className="d-none d-lg-flex justify-content-end mt-5">
-                        <Link to="/Article" className="card-link link-black fs-6 fw-bold">觀看文章<i className="bi bi-arrow-right text-primary ps-1"></i></Link>
+                      <div className="d-flex justify-content-end mt-5">
+                        <Link to="/Article" className="card-link link-black fs-7 fs-lg-6">觀看文章<i className="bi bi-arrow-right text-primary ps-1"></i></Link>
                       </div>
                     </div>
                   </div>
@@ -70,11 +71,11 @@ const Articles = () => {
                   </div>
                   <div className="col-lg-7">
                     <div className="card-body p-0 px-lg-5">
-                      <h5 className="card-title fs-6 fs-lg-3 fw-bold mb-0 mb-lg-7">三週養成習慣是迷思！做對這件事更重要</h5>
+                      <h5 className="card-title fs-6 fs-md-3 fw-bold mb-0 mb-lg-7">三週養成習慣是迷思！做對這件事更重要</h5>
                       <p className="card-text d-none d-lg-block fs-6 mb-7">過去，常聽研究指出「養成習慣需要 21 天」一說，只要三週時間就能夠培養出全新的好習慣。不過近年來的心理學研究指出，其實養成一個人真實的習慣平均要花 66 天，並因習慣的困難程度⋯⋯</p>
                       <div className="d-none d-lg-block border-bottom border-darkgray"></div>
-                      <div className="d-none d-lg-flex justify-content-end mt-5">
-                      <Link to="/Article" className="card-link link-black fs-6 fw-bold">觀看文章<i className="bi bi-arrow-right text-primary ps-1"></i></Link>
+                      <div className="d-flex justify-content-end mt-5">
+                        <Link to="/Article" className="card-link link-black fs-7 fs-lg-6">觀看文章<i className="bi bi-arrow-right text-primary ps-1"></i></Link>
                       </div>
                     </div>
                   </div>
@@ -96,11 +97,11 @@ const Articles = () => {
                   </div>
                   <div className="col-lg-7">
                     <div className="card-body p-0 px-lg-5">
-                      <h5 className="card-title fs-6 fs-lg-3 fw-bold mb-0 mb-lg-7">養成習慣前，如何建立能夠做到的目標？</h5>
+                      <h5 className="card-title fs-6 fs-md-3 fw-bold mb-0 mb-lg-7">養成習慣前，如何建立能夠做到的目標？</h5>
                       <p className="card-text d-none d-lg-block fs-6 mb-7">你能否養成新習慣、改變壞習慣的關鍵，取決於你自己是否真的「有決心」每天重複去做到新的目標。如果自己無法下定決心培養習慣，再多的工具和養成方式都是徒勞，本文將帶你一起探討⋯⋯</p>
                       <div className="d-none d-lg-block border-bottom border-darkgray"></div>
-                      <div className="d-none d-lg-flex justify-content-end mt-5">
-                      <Link to="/Article" className="card-link link-black fs-6 fw-bold">觀看文章<i className="bi bi-arrow-right text-primary ps-1"></i></Link>
+                      <div className="d-flex justify-content-end mt-5">
+                        <Link to="/Article" className="card-link link-black fs-7 fs-lg-6">觀看文章<i className="bi bi-arrow-right text-primary ps-1"></i></Link>
                       </div>
                     </div>
                   </div>
